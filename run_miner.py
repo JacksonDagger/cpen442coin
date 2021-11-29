1c36f5a0da36afb85cc60c46617fd1b3c0a8e913
Jackson Dagger <jacksonddagger@gmail.com>
2021-11-29 12:02:41 -0800

    fixed bug that could cause request spam in rare circumstances

--- run_miner.py
+++ run_miner.py
@@ -61,127 +61,136 @@ def main():
     dumpfileName = "logs/logdump" + datetime.now().strftime("%Y-%m-%d--%H-%M-%S") + ".log"
     stateFilename = "logs/state.json"
 
+    logdir = "logs"
+    if not os.path.exists(logdir):
+        os.makedirs(logdir)
+
     log = {}
     log["hashrate"] = 0
     log["coinrate"] = 0
     log["predcoins"] = 0
+    log["coinsmined"] = 9
+    log["lastCoin"] = "000000000039f30c6e714e3e4551f91cebea6cbbf42a0cbc0df4ea5d2c48debc"
+    log["difficulty"] = 10
+    log["foundBlob"] = ""
+    log["foundDif"] = 0
+    log["lastCoinTime"] = 0
 
-    lastCoin = "000000000039f30c6e714e3e4551f91cebea6cbbf42a0cbc0df4ea5d2c48debc"
-    evenDif = 8
-    difficulty = 10
-    foundBlob = ""
-    foundDif = 0
-    lastCoinTime = 0
     period = 0
-
-    log["coinsmined"] = 7
+    difDrop = 600*60
 
     try:
         with open(stateFilename, "r") as stateFile:
             statelog = json.load(stateFile)
             for key in statelog.keys():
                 log[key] = statelog[key]
-            lastCoin = log["lastcoin"]
-            foundBlob = log["foundBlob"]
-            foundDif = log["foundDif"]
-            # lastCoinTime = log["l"]
     except Exception as e:
         logEvent(dumpfileName, "json load error", str(e))
 
-    logdir = "logs"
-    if not os.path.exists(logdir):
-        os.makedirs(logdir)
-
     contestEnd = datetime(2021, 12, 1, 21, 0, 0)
     
-
     req_interval = 1
     req_num = 0
 
     while(True):
-        oldCoin = lastCoin
+        req_num += 1
+        with open(logfileName, "a") as logfile:
+            log["timestamp"] = datetime.now().strftime("%Y-%m-%d--%H:%M:%S")
+            logfile.write(json.dumps(log))
+            logfile.write("\n")
+
+        oldCoin = log["lastCoin"]
         if not (req_num % req_interval):
             try:
                 lastCoinResponseStr = requests.post("http://cpen442coin.ece.ubc.ca/last_coin")
-                lastCoin = lastCoinResponseStr.json()["coin_id"]
-                lastCoinTime = lastCoinResponseStr.json()["time_stamp"]
+                log["lastCoin"] = lastCoinResponseStr.json()["coin_id"]
+                log["lastCoinTime"] = lastCoinResponseStr.json()["time_stamp"]
 
                 difficultyResponseStr = requests.post("http://cpen442coin.ece.ubc.ca/difficulty")
-                difficulty = int(difficultyResponseStr.json()["number_of_leading_zeros"])
+                log["difficulty"] = int(difficultyResponseStr.json()["number_of_leading_zeros"])
             except Exception as e:
                 logEvent(dumpfileName, "server error", str(e) + " req interval:" + str(req_interval))
                 req_interval *= 2
                 pass
 
-        if lastCoin != oldCoin:
-            evenDif = difficulty - difficulty % 2
-            foundBlob = ""
-            foundDif = 0
+        period = 30*60 if log["difficulty"] >= 10 else 0
+        
+        if log["lastCoin"] != oldCoin:
+            logEvent(dumpfileName, "newcoin", "old: " + oldCoin + ", new: " + log["lastCoin"])
+            log["foundBlob"] = ""
+            log["foundDif"] = 0
             
-        if foundDif >= difficulty:
-            b64 = b64encode(bytes.fromhex(foundBlob)).decode()
+        if log["foundDif"] >= log["difficulty"]:
+            b64 = b64encode(bytes.fromhex(log["foundBlob"])).decode()
             data = {
             "coin_blob":b64,
             "id_of_miner":miner_id
             }
 
-            wait = period - (datetime.now().timestamp() - lastCoinTime)
+            wait = period - (datetime.now().timestamp() - log["lastCoinTime"])
+            if wait > 60:
+                wait = 60
             if wait > 0:
-                #logEvent(dumpfileName, "sleep", str(wait))
-                #sleep(wait)
-                pass
+                logEvent(dumpfileName, "close interval sleep", str(wait))
+                print("sleeping 60s")
+                sleep(wait)
+
             response = requests.post('http://cpen442coin.ece.ubc.ca/claim_coin', json = data)
-            
             log["coinsmined"] += 1
 
             data["code"] = response.status_code
+            if data["code"] == 200:
+                log["coinsmined"] += 1
+            else:
+                logEvent(dumpfileName, "bad response", str(response))
             data["coinsmined"] = log["coinsmined"]
             data["timestamp"] = datetime.now().strftime("%Y-%m-%d--%H:%M:%S")
 
             with open(coinlogfileName, "a") as coinlogfile:
                 coinlogfile.write(json.dumps(data))
                 coinlogfile.write("\n")
+                
+            continue
 
-        log["lastcoin"] = lastCoin
-        log["difficulty"] = difficulty
-        log["evenDif"] = evenDif
+        elif log["foundDif"] >= 10 and log["difficulty"] >= 11:
+            wait = int(log["lastCoinTime"] + difDrop - datetime.now().timestamp())
+            partwait = wait
+            if partwait > 122:
+                partwait = 122
+            partwait -= 2
+            if partwait > 0:
+                logEvent(dumpfileName, "high dif sleep ", "total: " + str(wait) + ", interval: " + str(partwait))
+                print("sleeping " + str(partwait) + " with total expected wait of: " + str(wait))
+                sleep(partwait)
+            if wait >= 0:
+                continue
 
         # args = [binpath, lastCoin, str(evenDif)]
-        args = [binpath, lastCoin]
+        args = [binpath, log["lastCoin"]]
         output = subprocess.run(args, capture_output=True)
         ret = output.stdout.decode()
 
         if "success:" in ret:
             newFoundBlob = ret[len("success:"):]
-            newFoundDif = difficulty_check(lastCoin, newFoundBlob)
+            newFoundDif = difficulty_check(log["lastCoin"], newFoundBlob)
 
-            if newFoundDif > foundDif:
-                foundDif = newFoundDif
-                foundBlob = newFoundBlob
+            if newFoundDif > log["foundDif"]:
+                log["foundDif"] = newFoundDif
+                log["foundBlob"] = newFoundBlob
             
-            if newFoundDif < 9: # evenDif:
+            if newFoundDif < 9:
                 blobLog = log
-                blobLog["foundBlob"] = foundBlob
-                blobLog["foundDif"] = foundDif
-                blobLog["hash"] = cpen442coinhash(lastCoin, foundBlob)
+                blobLog["hash"] = cpen442coinhash(log["lastCoin"], log["foundBlob"])
                 logEvent(dumpfileName, "bad blob", str(blobLog))
         else:
             log["hashrate"] = float(ret.split()[-1])
-            log["coinrate"] = 60*60*log["hashrate"]/(2**(4*difficulty))
+            log["coinrate"] = 60*60*log["hashrate"]/(2**(4*log["difficulty"]))
             hoursLeft = (contestEnd - datetime.now()) / timedelta(hours=1)
             log["predcoins"] = hoursLeft*log["coinrate"]
 
-        log["foundBlob"] = foundBlob
-        log["foundDif"] = foundDif
-
-        with open(logfileName, "a") as logfile:
-            log["timestamp"] = datetime.now().strftime("%Y-%m-%d--%H:%M:%S")
-            logfile.write(json.dumps(log))
-            logfile.write("\n")
-
         logEvent(dumpfileName, "stdout", ret)
         printLogStr(log)
-        req_num += 1
+        
         try:
             with open(stateFilename, "w") as stateFile:
                 json.dump(log, stateFile)
